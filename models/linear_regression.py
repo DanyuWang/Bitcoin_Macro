8cf3179773091776a77d11529b37be0fd0289bf6
DanyuWang <wangdanyu0098@gmail.com>
2022-01-12 17:03:49 +0100

    LR updated.

--- models/linear_regression.py
+++ models/linear_regression.py
@@ -51,14 +51,13 @@ def train_linearRegression(x_train, y_train, x_test, y_test):
         test_predict = model(test_inputs)
         test_loss_record.append(criterion(test_predict, test_labels).item())
 
-    plt.plot(test_loss_record)
-    plt.show()
-
     return train_loss_record, model
 
 
 def loop_k_fold(X, Y, k_fold=4):
     loss_k = []
+    loss_test_k = []
+    criterion = nn.HuberLoss()
     test_len, train_indices = k_fold_indice(len(X), test_size=0.1, k_fold=k_fold)
     for i in reversed(range(k_fold)):
         print("Training", i+1, "fold")
@@ -69,14 +68,18 @@ def loop_k_fold(X, Y, k_fold=4):
 
         loss_record, model = train_linearRegression(x_train, y_train, x_test, y_test)
         loss_k.append(loss_record[-1])
+        test_labels = Variable(torch.from_numpy(y_test.values)).float()
+        test_labels = test_labels.reshape(len(test_labels), 1)
         test_predict = model(Variable(torch.from_numpy(x_test.values)).float())
+        loss_test_k.append(criterion(test_predict, test_labels).item())
         test_predict = test_predict.reshape(-1)
 
-    return loss_record, test_predict, y_test, loss_k
+    return loss_record, test_predict, y_test, loss_k, loss_test_k
 
 
 def show_prediction(predict, label, if_cumprod=False):
     predict = pd.Series(predict.detach().numpy(), index=label.index)
+    print(np.corrcoef(predict, label))
     if if_cumprod:
         change_true = np.cumprod(1 + label)
         change_pred = np.cumprod(1 + predict)
@@ -100,5 +103,9 @@ if __name__ == '__main__':
     # y_train = Y.iloc[:train_indices[0]]
     #
     # train_linearRegression(x_train, y_train)
-    loss_record, test_predict, y_test, loss_k = loop_k_fold(X, Y)
+    loss_record, test_predict, y_test, loss_k, loss_k_test = loop_k_fold(X, Y)
     show_prediction(test_predict, y_test, if_cumprod=False)
+    print('Average train loss', np.mean(loss_k))
+    print('Average test loss', np.mean(loss_k_test))
+    plt.plot(loss_record)
+    plt.show()
